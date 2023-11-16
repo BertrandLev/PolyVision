1b2033de0f709d2329d8f3d39c17d5de13cc3a0f
BertrandLev <138677458+BertrandLev@users.noreply.github.com>
2023-11-16 12:06:41 +0100

    modif pandasmodel

--- tests/test_divers.py
+++ tests/test_divers.py
@@ -59,11 +59,10 @@ class groupPandasModel(QStandardItemModel):
         def is_key_exist(parent:QStandardItem, key:str) -> QStandardItem:
             # si la clef existe, on la renvoi, sinon on la créé et on la renvoi
             for row in range(parent.rowCount()):
-                idx = self.index(row,0,parent)
-                item = self.data(idx,Qt.ItemDataRole.DisplayRole)
-                if item is not None and item.text()==key:
+                item = parent.child(row,0)
+                if item.text()==key:
                     return item
-            new_key = QStandardItem()
+            new_key = QStandardItem(str(key))
             new_key.setData(key,Qt.ItemDataRole.DisplayRole)
             parent.appendRow(new_key)
             return new_key
@@ -74,7 +73,7 @@ class groupPandasModel(QStandardItemModel):
                 add_key(key,keys[1:],values)
             else:
                 key = is_key_exist(parent,keys[0])
-                for row,cols in values.iterrows():
+                for _,cols in values.iterrows():
                     items = [QStandardItem("")]
                     for col in values.columns.values:
                         if col not in columns:
@@ -85,6 +84,8 @@ class groupPandasModel(QStandardItemModel):
             
 
         for keys, group in data.groupby(columns):
+            print(keys)
+            print(group)
             add_key(self.invisibleRootItem(),keys,group)
 
         header_values = [""] + [col for col in data.columns.values if col not in columns]
@@ -95,7 +96,7 @@ class MultiLevelHeaderExample(QWidget):
         super().__init__()
 
         data = [["sample",4,5,6],
-                ["model",45,67,90],
+                ["model",9,67,90],
                 ["sample",4,45,9],
                 ["model",3,5,7],
                 ["sample",5,34,5]]
@@ -130,41 +131,20 @@ class MultiLevelHeaderExample(QWidget):
         group_model = model.GroupbyColumnTableModel()
         group_model.setSourceModel(table_model)
 
-        data = [["sample",4,5,6],
-                ["model",45,67,90],
-                ["sample",4,45,9],
-                ["model",3,5,7],
-                ["sample",5,34,5]]
-        
-        # model pandas
-        data_df = pd.DataFrame(data, columns=["colA","colB","colC","colD"])
-        model_pandas = pandasModel(data_df)
-        
-        group_model_pandas = groupPandasModel(data_df,["colA","colB"])
+        # Creation des vues
+        self.layout = QVBoxLayout()
+        self.table = QTableView()
+        self.table.setModel(model_pandas)
+        self.tree = QTreeView()
+        self.tree.setModel(group_model_pandas)
+        self.tree.expandAll()
+        self.layout.addWidget(self.table)
+        self.layout.addWidget(self.tree)
+        self.setLayout(self.layout)
 
-        # Creation du model table
-        table_model = QStandardItemModel(4,4)
-        for row in range(table_model.rowCount()):
-            for col in range(table_model.columnCount()):
-                idx = table_model.index(row,col)
-                table_model.setData(idx,data[row][col],QtCore.Qt.ItemDataRole.DisplayRole)
-
-        #Creation du model arbre
-        tree_model = QStandardItemModel()
-        for row, columns in enumerate(data):
-            root = QStandardItem()
-            root.setData(data[row][0],QtCore.Qt.ItemDataRole.DisplayRole)
-            tree_model.appendRow(root)
-            values = []
-            for col in columns[1:]:
-                item = QStandardItem()
-                item.setData(col,QtCore.Qt.ItemDataRole.DisplayRole)
-                values.append(item)
-            root.appendRow(values)
-        tree_model.setHorizontalHeaderLabels(["1","2","3"])
-
-        group_model = model.GroupbyColumnTableModel()
-        group_model.setSourceModel(table_model)
+        #Test
+        idx = group_model_pandas.index(1,0,group_model_pandas.index(0,0))
+        print(group_model_pandas.data(idx,Qt.ItemDataRole.DisplayRole))
 
     
 app = QApplication([])
