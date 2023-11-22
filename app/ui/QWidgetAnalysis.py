450116d366e23853a7610814dff661904df3981f
BertrandLev <138677458+BertrandLev@users.noreply.github.com>
2023-11-22 15:13:32 +0100

    add the pivot table in analysis tab

--- app/ui/QWidgetAnalysis.py
+++ app/ui/QWidgetAnalysis.py
@@ -9,14 +9,15 @@ class AnalysisTab(QWidget):
         self.lims_data = pd.DataFrame()
         self.lims_table_data = PandasModel(self.lims_data)
 
-        self.query_result = QTableView()
-        self.query_result.setModel(self.lims_table_data)
+        self.pivot_result = QTableView()
+        self.pivot_result.setModel(self.lims_table_data)
 
         layout = QVBoxLayout()
-        layout.addWidget(self.query_result)
+        layout.addWidget(self.pivot_result)
         # Add widgets to the Selection tab
         self.setLayout(layout)
 
     def update_df_data(self, df_data:pd.DataFrame):
-        self.lims_data = df_data
+        self.lims_data = df_data.pivot(index=["PRODUCT","MATERIAL_NAME","SAMPLE_NUMBER","REPLICATE"],
+                                   columns="RESULT_NAME",values="RESULT").reset_index()
         self.lims_table_data.set_dataFrame(self.lims_data)
